5cf637111a27a7aa5ba4721af18d6a707ea762bc
ppgod123 <3343674462@qq.com>
2022-12-15 18:26:51 +0800

    构造函数练习

--- src/main/com.aj.Day03/Constructor.java
+++ src/main/com.aj.Day03/Constructor.java
@@ -5,6 +5,30 @@ public class Constructor {
     public String name;
     public int age;
     public String gender;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     public Constructor(){
 
