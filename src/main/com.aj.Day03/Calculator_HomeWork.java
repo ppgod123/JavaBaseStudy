453674af074fdcdc7e7075802f784a375b26647f
ppgod123 <3343674462@qq.com>
2022-12-19 15:52:52 +0800

    构造函数练习

--- src/main/com.aj.Day03/Calculator_HomeWork.java
+++ src/main/com.aj.Day03/Calculator_HomeWork.java
@@ -1,26 +1,44 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class Calculator_HomeWork {
 
-    public static double add(double a, double b){
-        return a+b;
+    public  BigDecimal add(BigDecimal a, BigDecimal b){
+        return a.add(b);
     }
-    public static double minus(double a, double b){
-        return a-b;
+    public  BigDecimal subtract(BigDecimal a, BigDecimal b){
+        return a.subtract(b);
     }
-    public static double multiply(double a, double b){
-        return a*b;
+    public  BigDecimal multiply(BigDecimal a, BigDecimal b){
+        return a.multiply(b);
     }
-    public static BigDecimal divide(BigDecimal a, BigDecimal b){
+    public  BigDecimal divide(BigDecimal a, BigDecimal b){
         return a.divide(b,30,BigDecimal.ROUND_CEILING);
     }
 
     public static void main(String[] args) {
-        System.out.println(add(3,4));
-        System.out.println(minus(3,12));
-        System.out.println(multiply(3,12));
-        BigDecimal aa = new BigDecimal(3);
-        BigDecimal bb = new BigDecimal(7);
-        System.out.println(divide(aa,bb));
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("请输入第一个数字：");
+            BigDecimal a = scanner.nextBigDecimal();
+            System.out.println("输入内容必须为数字！！！");
+            System.out.print("请输入运算符：");
+            String C = scanner.next();
+            System.out.println("输入字符必须为*、/、-、+");
+            System.out.print("请输入第二个数字：");
+            BigDecimal b = scanner.nextBigDecimal();
+            System.out.println("输入内容必须为数字！！！");
+        Calculator_HomeWork calculator_homeWork = new Calculator_HomeWork();
+        if ("+".equals(C)){
+            System.out.println(calculator_homeWork.add(a,b));
+        }else if ("-".equals(C)){
+            System.out.println(calculator_homeWork.subtract(a,b));
+        }else if ("*".equals(C)){
+            System.out.println(calculator_homeWork.multiply(a,b));
+        }else if ("/".equals(C)){
+            System.out.println(calculator_homeWork.divide(a,b));
+        }
+        scanner.close();
     }
 }
